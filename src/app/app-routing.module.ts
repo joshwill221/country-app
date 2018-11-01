b589690e9cf2a0276e96139e1a44c7bbc12856e4
joshwill221 <joshwill221@gmail.com>
2018-11-01 14:19:28 +0800

    Implemented Routing so each page can be traversed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',           component: HomeComponent },
+  { path: 'detail/:name',   component: CountryDetailComponent },
+  { path: 'all-countries',  component: AllCountriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Country } from '../country';
