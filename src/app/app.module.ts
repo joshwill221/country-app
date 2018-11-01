c8e6d379dcbff3186361a993e42c4cf2757584f8
joshwill221 <joshwill221@gmail.com>
2018-11-01 12:42:47 +0800

    Initial setup & added a Countries data service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CountryService } from './country.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CountryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/country.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { Country } from './country';
+import { COUNTRIES } from './country-data';
+
+@Injectable()
+export class CountryService {
+
+    constructor() { }
+
+    getCountries(): Country[] {
+        return COUNTRIES;
+    }
+
+    getPopulatedCountried(): Country[] {
+        // b.area - a.area returns descending order.
+        // .slice(0, 3) returns the first three countries.
+        return COUNTRIES.sort((a, b) => b.population - a.population).slice(0, 3);
+    }
+
+    getLargestCountries(): Country[] {
+        return COUNTRIES.sort((a, b) => b.area - a.area).slice(0, 3);
+    }
+
+    getGPDCountries(): Country[] {
+        return COUNTRIES.sort((a, b) => b.gdp - a.gdp).slice(0, 3);
+    }
+
+    getCountry(name: string): Country {
+        return COUNTRIES.find(country => country.name === name);
+    }
+}
