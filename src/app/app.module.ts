404445841d5bc5d1c82efe572af6819688a75b08
joshwill221 <joshwill221@gmail.com>
2018-11-01 14:06:30 +0800

    Added AllCountriesComponent and CountryDetailComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 
 import { CountryService } from './country.service';
 import { HomeComponent } from './home/home.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AllCountriesComponent,
+    CountryDetailComponent
   ],
   imports: [
     BrowserModule,
